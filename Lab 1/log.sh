e94649cc3379a0f04bfc0733b3478fe8234afce8
braindeaead <shkahov.azamat@yandex.ru>
2020-09-24 17:33:37 +0300

    fixed log.sh

--- Lab 1/log.sh
+++ Lab 1/log.sh
@@ -3,7 +3,7 @@
 log() {
 
     warnings="$(cat /var/log/anaconda/X.log | awk '{if (($3 == "(WW)") && ($1 == "["))print $0}')"
-    infos="$(cat /var/log/anaconda/X.log | awk '{if (($3 == "(II)") && ($1 == "["))printf $0}')"
+    infos="$(cat /var/log/anaconda/X.log | awk '{if (($3 == "(II)") && ($1 == "["))print $0}')"
 
     echo -e "${warnings//"(WW)"/"\e[1;33Warning:\e[0m"}"
     echo -e "${infos//"(II)"/"\e[1;36Information:\e[0m"}"
