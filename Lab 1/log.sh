b2d129bf9c2a565a0f66002985f9a62afb2c6135
braindeaead <shkahov.azamat@yandex.ru>
2020-09-24 14:02:39 +0300

    added script for log.sh

--- Lab 1/log.sh
+++ Lab 1/log.sh
@@ -1,2 +1,10 @@
 #!/bin/zsh
 
+log() {
+
+    warnings="$(cat /var/log/anaconda/X.log | awk '{if (($3 == "(WW)") && ($1 == "["))print $0}')"
+    infos="$(cat /var/log/anaconda/X.log | awk '{if (($3 == "(II)") && ($1 == "["))printf $0}')"
+
+    echo -e "${warnings//"(WW)"/"\e[1;33Warning:\e[0m"}"
+    echo -e "${infos//"(II)"/"\e[1;36Information:\e[0m"}"
+}
