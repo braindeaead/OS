a874f82730af83c1b457d0207235dabb80db0869
braindeaead <shkahov.azamat@yandex.ru>
2020-09-24 14:32:30 +0300

    fixed smth syntax

--- Lab 1/calc.sh
+++ Lab 1/calc.sh
@@ -3,9 +3,9 @@
 calc () {
 
     case $1 in
-        sum) echo (($2 + $3));;
-        sub) echo (($2 - $3));;
-        mul) echo (($2 * $3));;
-        div) echo (($2 / $3));;
+        sum) echo $(($2 + $3));;
+        sub) echo $(($2 - $3));;
+        mul) echo $(($2 * $3));;
+        div) echo $(($2 / $3));;
     esac
 }
